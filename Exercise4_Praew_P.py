089d8b30721396603996d3e827a4e96469952176
Praew-pi <praew0018@gmail.com>
2020-04-22 23:22:11 +0700

    Exercise4

---
+++ Exercise4_Praew_P.py
@@ -0,0 +1,9 @@
+Fund_Eng_Score = 45
+Gen_Bus_Score = 68.9
+Intro_Com_Score = 89.45
+Com_Score = 70
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้", Fund_Eng_Score,)
+print("General Business : คะแนนที่ได้", Gen_Bus_Score, )
+print("Introduction to Computer Systems : คะแนนที่ได้", Intro_Com_Score,)
+print("Computer Programming : คะแนนที่ได้", Com_Score, )
