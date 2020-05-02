c56390e0a92512498f27110dcd90af42349bf8fe
Praew-pi <praew0018@gmail.com>
2020-05-02 23:07:43 +0700

    Lecture50

---
+++ Lecture50_Praew_P.py
@@ -0,0 +1,15 @@
+def Plus(x,y):
+    print(x, "+", y, "=", x+y)
+def Minus(x,y):
+    print(x, "-", y, "=", x-y)
+def Muliplied(x,y):
+    print(x, "x", y, "=", x*y)
+def diivide(x,y):
+    print(x, "/", y, "=", x/y)
+
+x = int(input("Please input x:"))
+y = int(input("Please input y:"))
+Plus(x,y)
+Minus(x,y)
+Muliplied(x,y)
+diivide(x,y)
